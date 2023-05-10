86b8fbf475e5552832ec0c62266f0c84549e89af
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-05-11 01:17:13 +0530

    post method handled

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    result: tours.length,
     data: {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.end('Done');
 });
 
 const port = 3000;
