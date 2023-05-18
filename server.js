fb5917d892ac98e036674b5a6a2639dcd80b84fb
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-05-18 21:45:53 +0530

    data schema created but DB is not connected

--- server.js
+++ server.js
@@ -2,6 +2,43 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const { default: mongoose } = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    // validators
+    type: String,
+    required: true,
+    unique: true,
+  },
+  rating: {
+    // validators
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    // validators
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 470,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error💥 : ', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
